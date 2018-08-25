15a73aa7184faa0c4976562c472eac79d8acfaad
rizkyalviandra <rizkyalviandra@gmail.com>
2018-08-25 11:21:31 +0700

    Website CCTV Terminal Type B

---
+++ application/controllers/Login.php
@@ -0,0 +1,131 @@
+<?php
+
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class Login extends CI_Controller {
+    
+        public function __construct()
+        {
+            parent::__construct();
+            
+            $this->load->helper('url','form');
+            $this->load->library('form_validation');
+            $this->load->model('Login_model'); 
+        }
+
+        public function index()
+        {
+           
+                $this->load->view('login/login');
+            
+        }
+
+        
+
+        public function cekLogin()
+        {
+            // $this->load->library('form_validation');
+            // $this->form_validation->set_rules('username', 'Username', 'trim|required');
+		    // $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+            // $user = "admin";
+            // $pass = "admin";
+
+            // $user2 = "adminmavens";
+            // $pass2 = "adminmavens";
+            
+            // if ($this->form_validation->run() == FALSE) {
+                
+            //     $this->load->view('login/login');
+                
+            // } else {
+
+            //     if(isset($_POST["submit"])) {
+            //         if($_POST["username"] == $user && $_POST["password"] == $pass) {
+                        
+            //             redirect('Home','refresh');
+                        
+            //         }
+            //         elseif($_POST["username"] == $user2 && $_POST["password"] == $pass2) {
+                        
+            //             redirect('HomeMav','refresh');
+                        
+            //         } else {
+                        
+            //             $this->load->view('login/login');
+                        
+            //             $this->form_validation->set_message("cekDb", "Login Gagal Username dan Password tidak valid");
+            //             return false;
+                        
+            //         }
+            //     }
+            // }
+
+            // $this->load->library('form_validation');
+            
+		    $username=htmlspecialchars($this->input->post('username',TRUE),ENT_QUOTES);
+            $password=htmlspecialchars($this->input->post('password',TRUE),ENT_QUOTES);
+           
+            $cek_admin=$this->Login_model->admin($username,$password);
+
+            if($cek_admin->num_rows() > 0){
+                $data=$cek_admin->row_array();
+                $this->session->set_userdata('masuk',TRUE);
+                    $this->session->set_userdata('akses','1');
+                    $this->session->set_userdata('ses_nama',$data['username']);
+                    redirect('home');
+            }else{
+                $cek_mavens=$this->Login_model->adminmavens($username,$password);
+                    if($cek_mavens->num_rows() > 0){
+                        $data=$cek_mavens->row_array();
+                        $this->session->set_userdata('masuk',TRUE);
+                        $this->session->set_userdata('akses','2');
+                        $this->session->set_userdata('ses_nama',$data['username']);
+                        redirect('HomeMav');
+                    }else{
+                        $url=base_url();
+                        echo $this->session->set_flashdata('msg','Username Atau Password Salah');
+                        redirect($url);
+                    }
+            }
+            
+	    }
+        
+
+        // public function cekDb($password)
+        // {
+        //     $username = $this->input->post('username');
+		//     $result = $this->Login_model->login($username, $password);
+		//     if ($result) {
+		// 	    $sess_array = array();
+		// 	    foreach ($result as $row) {
+		// 		    $sess_array = array(
+		// 			    'id_user' =>$row->id_user,
+		// 			    'username' => $row->username,
+		// 			    // 'role' => $row->role
+		// 		    );
+				
+		// 		    $this->session->set_userdata( 'logged_in',$sess_array );
+		// 	    }
+		// 	    return true;
+		//     }else {
+		// 	    $this->form_validation->set_message("cekDb", "Login Gagal Username dan Password tidak valid");
+		// 	    return false;
+		//     }
+        // }
+
+ 
+        public function logout()
+        {
+            $this->session->unset_userdata('logged_in');
+		    $this->session->sess_destroy();
+		    redirect('Login','refresh');
+        }
+    
+    }
+    
+    
+    
+    /* End of file Login.php */
+    
+?>
